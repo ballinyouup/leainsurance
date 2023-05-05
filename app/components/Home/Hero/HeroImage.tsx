665d295b08f53f90340d4807be975e3e1e560cfa
ballinyouup <bryanhuici@gmail.com>
2023-05-05 03:15:49 -0400

    fixed image optimization

--- app/components/Home/Hero/HeroImage.tsx
+++ app/components/Home/Hero/HeroImage.tsx
@@ -6,9 +6,8 @@ const HeroImage = () => {
 			className="absolute -top-20 h-[680px] w-fit self-end object-cover object-[-150px_0px] brightness-[35%] sm:brightness-[35%] md:object-[0px_0px] md:brightness-[50%] lg:object-[0px_0px] xl:w-2/3 xl:object-[0px_0px]"
 			src="/landingImage.png"
 			alt="Landing Page Image"
-			width={800}
-			height={940}
-			unoptimized
+			width={2000}
+			height={680}
 		/>
 	);
 };
