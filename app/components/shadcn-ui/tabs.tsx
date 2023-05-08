a753758f7d09c72a7d13571a5c82e9470fcfa95b
ballinyouup <bryanhuici@gmail.com>
2023-05-08 17:32:11 -0400

    styling

--- app/components/shadcn-ui/tabs.tsx
+++ app/components/shadcn-ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
 	<TabsPrimitive.List
 		ref={ref}
 		className={cn(
-			"inline-flex h-10 items-center justify-center bg-primary p-2 text-white",
+			"inline-flex h-10 items-center justify-center bg-primary p-2",
 			className
 		)}
 		{...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
 	<TabsPrimitive.Trigger
 		ref={ref}
 		className={cn(
-			"inline-flex items-center justify-start whitespace-nowrap px-3 py-1.5 text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+			"inline-flex items-center justify-start whitespace-nowrap px-3 py-1.5 text-base font-medium text-primary-foreground ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
 			className
 		)}
 		{...props}
