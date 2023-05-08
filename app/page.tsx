ef88f0c1df44dc20b41b533e5a1b70a66094b68d
ballinyouup <bryanhuici@gmail.com>
2023-05-08 17:30:07 -0400

    refactored home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "./components/home/home-hero";
 import TextSection from "./components/home/home-text";
 import { Metadata } from "next";
-import ContactSection from "./components/home/home-contact";
+import Contact from "./components/home/home-contact";
 import WhySection from "./components/home/home-why";
-import Divider from "./components/ui/divider";
+import { Separator } from "$/separator";
 import Image from "next/image";
 import Cards from "./components/home/home-card";
+import Waves from "./components/home/home-waves";
 export const metadata: Metadata = {
 	title: "L.E.A Insurance",
 	description:
@@ -21,14 +22,16 @@ export default function Page() {
 			<div className="relative flex h-full w-full flex-col overflow-clip pb-6 sm:pb-0 md:max-w-9xl">
 				<Hero />
 				<TextSection />
-				<Divider />
+				<div className="flex w-full items-center justify-center">
+					<Separator className="hidden max-w-xl lg:block" />
+				</div>
 				<Cards />
 				<WhySection />
 				<section className="flex w-full flex-col items-center justify-center gap-8 px-5 pt-8 lg:p-8">
 					<span className="z-10 w-full max-w-md rounded-xl bg-teal-900 p-4 text-center text-4xl font-bold uppercase text-white shadow-[5px_5px_5px] shadow-black">
 						Meet your Agents
 					</span>
-					<div className="z-10 flex w-fit max-w-5xl flex-col items-center gap-12 rounded-lg bg-white p-8 lg:w-full lg:flex-row">
+					<div className="z-10 flex w-fit max-w-5xl flex-col items-center gap-12 rounded-lg bg-background p-8 lg:w-full lg:flex-row">
 						<Image
 							src={"/delia.jpg"}
 							alt={"Delia Huici Picture"}
@@ -38,7 +41,7 @@ export default function Page() {
 						/>
 						<div className="w-full max-w-md lg:max-w-lg">
 							<h3 className="text-2xl font-bold">Delia Huici</h3>
-							<span className="text-base text-black sm:text-lg lg:w-192">
+							<span className="text-base text-primary sm:text-lg lg:w-192">
 								Meet Delia Huici, a licensed insurance agent with expertise in
 								Life Insurance, Medicare/Medicaid, Obamacare, and Funeral
 								Assistance. With over 20 years of experience in the healthcare
@@ -51,18 +54,9 @@ export default function Page() {
 						</div>
 					</div>
 				</section>
-				<Divider white />
-				<ContactSection />
-				<Image
-					src="/wave1.svg"
-					alt="waves"
-					width={1000}
-					height={800}
-					className="absolute -left-80 top-0 z-0 hidden sm:top-[500px] sm:block lg:-left-0"
-				/>
-				<div className="absolute -right-48 z-0 hidden w-[600px] overflow-clip sm:top-[875px] lg:block">
-					<Image src="/wave2.svg" alt="waves" width={800} height={600} />
-				</div>
+				<Separator />
+				<Contact />
+				<Waves />
 			</div>
 		</main>
 	);
