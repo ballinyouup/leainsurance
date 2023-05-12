0c89b6aa1ef096fb6030c4ab7b7d115a834ab952
ballinyouup <bryanhuici@gmail.com>
2023-05-12 19:32:46 -0400

    added new env keys

--- app/env.mjs
+++ app/env.mjs
@@ -4,15 +4,17 @@ import { z } from "zod";
 export const env = createEnv({
 	clientPrefix: "NEXT_PUBLIC",
 	server: {
-		POSTGRES_URL: z.string().url(),
+		DATABASE_URL: z.string().url(),
 		CLERK_SECRET_KEY: z.string().min(1),
 		ADMIN_ID: z.string().min(1),
+		NODE_ENV: z.string(),
 	},
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
 	},
 	runtimeEnv: {
-		POSTGRES_URL: process.env.POSTGRES_URL,
+		NODE_ENV: process.env.NODE_ENV,
+		DATABASE_URL: process.env.DATABASE_URL,
 		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 		ADMIN_ID: process.env.ADMIN_ID,
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
