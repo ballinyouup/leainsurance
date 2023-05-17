9a3b80d8758837a2c8f1a9731a9d0f0890c7ce3c
ballinyouup <bryanhuici@gmail.com>
2023-05-16 21:02:35 -0400

    added aspect ratio to images

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { Separator } from "$/separator";
 import Image from "next/image";
 import Cards from "./components/home/home-card";
 import Waves from "./components/home/home-waves";
+import { AspectRatio } from "./components/shadcn-ui/aspect-ratio";
 export const metadata: Metadata = {
 	title: "L.E.A Insurance",
 	description:
@@ -29,28 +30,76 @@ export default function Page() {
 				<WhySection />
 				<section className="flex w-full flex-col items-center justify-center gap-8 px-5 pt-8 lg:p-8">
 					<span className="z-10 w-full max-w-md rounded-xl bg-teal-900 p-4 text-center text-4xl font-bold uppercase text-white shadow-[5px_5px_5px] shadow-black">
-						Meet your Agents
+						Meet The Team
 					</span>
-					<div className="z-10 flex w-fit max-w-5xl flex-col items-center gap-12 rounded-lg bg-background p-8 lg:w-full lg:flex-row">
-						<Image
-							src={"/delia.jpg"}
-							alt={"Delia Huici Picture"}
-							width={200}
-							height={200}
-							className="rounded-lg"
-						/>
-						<div className="w-full max-w-md lg:max-w-lg">
-							<h3 className="text-2xl font-bold">Delia Huici</h3>
-							<span className="text-base text-primary sm:text-lg lg:w-192">
-								Meet Delia Huici, a licensed insurance agent with expertise in
-								Life Insurance, Medicare/Medicaid, Obamacare, and Funeral
-								Assistance. With over 20 years of experience in the healthcare
-								industry, Delia is dedicated to helping clients obtain the best
-								insurance coverage possible. Trustworthy and knowledgeable, she
-								can assist you in choosing the right insurance plan and provide
-								guidance with claims. Contact Delia today to learn more about
-								her services.
-							</span>
+					<div className="z-10 grid h-fit w-fit grid-cols-2 items-start gap-8 rounded-lg bg-background p-6 lg:grid-cols-4">
+						<div className="flex flex-col gap-4">
+							<AspectRatio ratio={3 / 4}>
+								<Image
+									src={"/LuisaPolanco.jpg"}
+									alt={"Luisa Polanco Picture"}
+									fill
+									className="rounded-lg"
+									quality={100}
+								/>
+							</AspectRatio>
+							<div>
+								<h3 className="text-xl font-bold">Luisa Polanco</h3>
+								<span className="text-base text-primary sm:text-lg lg:w-192">
+									VP of Sales & Marketing
+								</span>
+							</div>
+						</div>
+						<div className="flex flex-col gap-4">
+							<AspectRatio ratio={3 / 4}>
+								<Image
+									src={"/delia.jpg"}
+									alt={"Delia Huici Picture"}
+									fill
+									className="rounded-lg"
+									quality={100}
+								/>
+							</AspectRatio>
+							<div>
+								<h3 className="text-xl font-bold">Delia Huici</h3>
+								<span className="text-base text-primary sm:text-lg lg:w-192">
+									Manager of Sales & Marketing
+								</span>
+							</div>
+						</div>
+						<div className="flex flex-col gap-4">
+							<AspectRatio ratio={3 / 4}>
+								<Image
+									src={"/KimberlyCruz.jpg"}
+									alt={"Kimberly Cruz Picture"}
+									fill
+									className="rounded-lg"
+									quality={100}
+								/>
+							</AspectRatio>
+							<div>
+								<h3 className="text-xl font-bold">Kimberly Cruz</h3>
+								<span className="text-base text-primary sm:text-lg lg:w-192">
+									Agent
+								</span>
+							</div>
+						</div>
+						<div className="flex flex-col gap-4">
+							<AspectRatio ratio={3 / 4}>
+								<Image
+									src={"/AshleyLegros.jpg"}
+									alt={"Ashley Legros Picture"}
+									fill
+									className="rounded-lg"
+									quality={100}
+								/>
+							</AspectRatio>
+							<div>
+								<h3 className="text-xl font-bold">Ashley Legros</h3>
+								<span className="text-base text-primary sm:text-lg lg:w-192">
+									Agent
+								</span>
+							</div>
 						</div>
 					</div>
 				</section>
