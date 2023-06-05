361c4abb62cdf8ca264065b0c019e5a9c7bc803e
ballinyouup <bryanhuici@gmail.com>
2023-06-05 12:04:19 -0400

    added blog functionality

---
+++ app/(lang)/(en)/(routes)/blog/[slug]/page.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
+import { createClient } from "next-sanity";
+import { PortableText } from "@portabletext/react";
+import urlBuilder from "@sanity/image-url";
+const client = createClient({
+	projectId: "be20oshw",
+	dataset: "production",
+	apiVersion: "2023-06-05",
+	useCdn: false,
+});
+
+const ImageComponent = ({ value }: any) => {
+	return (
+		<img
+			src={urlBuilder(client)
+				.image(value)
+				.width(400)
+				.fit("max")
+				.auto("format")
+				.url()}
+			loading="lazy"
+			alt="image"
+		/>
+	);
+};
+
+const components = {
+	block: {
+		h1: ({ children }: any) => <h1 className="text-4xl">{children}</h1>,
+	},
+	listItem: {
+		bullet: ({ children }: any) => (
+			<li className="ml-4 list-disc">{children}</li>
+		),
+	},
+	types: {
+		image: ImageComponent,
+	},
+};
+
+export default async function Page({ params }: { params: { slug: string } }) {
+	const post = await client.fetch(`*[slug.current == "${params.slug}"]`);
+	return (
+		<div className="h-full w-full p-8">
+			{post && <PortableText value={post[0]?.body} components={components} />}
+		</div>
+	);
+}
