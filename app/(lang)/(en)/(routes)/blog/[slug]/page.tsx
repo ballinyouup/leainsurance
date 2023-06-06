84bbd87158c5f46a898f3299c22fffd61395b071
ballinyouup <bryanhuici@gmail.com>
2023-06-06 10:49:31 -0400

    added not found check

--- app/(lang)/(en)/(routes)/blog/[slug]/page.tsx
+++ app/(lang)/(en)/(routes)/blog/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "next-sanity";
 import { PortableText } from "@portabletext/react";
 import urlBuilder from "@sanity/image-url";
 import { type SanityImageSource } from "@sanity/image-url/lib/types/types";
+import Link from "next/link";
 const client = createClient({
 	projectId: "be20oshw",
 	dataset: "production",
@@ -68,17 +69,28 @@ export default async function Page({ params }: { params: { slug: string } }) {
 		"author": author->name,
 		  body, title
 	  }`);
-
+	if (post.length === 0) {
+		return (
+			<div className="relative h-full">
+				<div className="absolute left-1/2 top-1/3 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-2">
+					<span className="text-5xl">Not Found</span>
+					<Link href="/blog" className="text-lg underline">
+						Return to Blog Posts
+					</Link>
+				</div>
+			</div>
+		);
+	}
 	return (
 		<div className="flex w-full flex-col items-center p-4">
 			<div className="flex w-full max-w-5xl flex-col">
 				<div className="py-4">
-					<h1 className="text-5xl font-bold capitalize">{post[0].title}</h1>
-					<p className="text-lg">By: {post[0].author}</p>
+					<h1 className="text-5xl font-bold capitalize">{post[0]?.title}</h1>
+					<p className="text-lg">By: {post[0]?.author}</p>
 				</div>
 				{post ? (
 					<PortableText
-						value={post[0].body ?? {}}
+						value={post[0]?.body ?? {}}
 						components={components}
 						onMissingComponent={false}
 					/>
